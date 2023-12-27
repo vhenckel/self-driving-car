71ea9319d1964e88fc05c9c600cec0281c342152
Vitor Henckel <vitor@henckel.com.br>
2023-12-27 12:37:46 -0300

    feat: add project self driving with javascript only

---
+++ main.js
@@ -0,0 +1,83 @@
+const carCanvas = document.getElementById("carCanvas");
+carCanvas.width = 300;
+const networkCanvas = document.getElementById("networkCanvas");
+networkCanvas.width = 400;
+
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+
+const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9);
+
+const N = 200;
+const cars = generateCars(N);
+let bestCar = cars[0];
+
+if (localStorage.getItem("bestBrain")) {
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].brain = JSON.parse(localStorage.getItem("bestBrain"));
+    if (i != 0) {
+      NeuralNetwork.mutate(cars[i].brain, 0.1);
+    }
+  }
+}
+
+const traffic = [
+  new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(0), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -300, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(0), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(1), -500, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(1), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+  new Car(road.getLaneCenter(2), -700, 30, 50, "DUMMY", 2, getRandomColor()),
+];
+
+animate();
+
+function generateCars(N) {
+  const cars = [];
+  for (let i = 1; i <= N; i++) {
+    cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+  }
+  return cars;
+}
+
+function save() {
+  localStorage.setItem("bestBrain", JSON.stringify(bestCar.brain));
+}
+
+function discard() {
+  localStorage.removeItem("bestBrain");
+}
+
+function animate(time) {
+  for (const element of traffic) {
+    element.update(road.borders, []);
+  }
+  for (const element of cars) {
+    element.update(road.borders, traffic);
+  }
+  bestCar = cars.find((car) => car.y == Math.min(...cars.map((c) => c.y)));
+
+  carCanvas.height = window.innerHeight;
+  networkCanvas.height = window.innerHeight;
+
+  carCtx.save();
+  carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
+
+  road.draw(carCtx);
+
+  for (const element of traffic) {
+    element.draw(carCtx, "red");
+  }
+  carCtx.globalAlpha = 0.2;
+  for (const element of cars) {
+    element.draw(carCtx, "blue");
+  }
+  carCtx.globalAlpha = 1;
+  bestCar.draw(carCtx, "blue", true);
+
+  carCtx.restore();
+  networkCtx.lineDashOffset = -time / 50;
+  Visualizer.drawNetwork(networkCtx, bestCar.brain);
+  requestAnimationFrame(animate);
+}
